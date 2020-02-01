80090a3362e5e18cc57200c00193325228f8eb2e
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-02-01 23:52:15 +0100

    cleaning code

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "archive_file" "zip" {
   type        = "zip"
-  source_file = "hello_lambda.py"
-  output_path = "hello_lambda.zip"
+  source_file = "terraform_lambda.py"
+  output_path = "terraform_lambda.zip"
 }
 
 data "aws_iam_policy_document" "policy" {
